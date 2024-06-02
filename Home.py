de683a598e4f30d4ff512212742d1e9fc0b8acaf
Helenita <helen.vargas.1205@gmail.com>
2024-06-02 16:00:00 -0500

    Home 5

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 
-import streamlit as st
-import matplotlib.pyplot as plt
+import streamlit as st # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 
 # Set the page title and header
 st.title("ESTADISTICAS CTECNO")
