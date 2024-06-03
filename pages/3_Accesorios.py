311fa19262d019e67db451dc80a3ea36499bd5ff
Helenita <helen.vargas.1205@gmail.com>
2024-06-03 17:09:54 -0500

    fIlteACC01

---
+++ pages/3_Accesorios.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+st.title("ESTADISTICA DE ACCESORIOS")
+
+df = pd.read_csv('static/datasets/Accesorios2.csv', parse_dates=['HORA'])
+
+DIA = st.multiselect('DIA', sorted(set(df['DIA'].unique())))
+HORA = st.multiselect('HORA', sorted(set(df['HORA'].dt.strftime('%H:%M').unique())))
+
+df_filtrado = df[df['DIA'].isin(DIA) & df['HORA'].dt.strftime('%H:%M').isin(HORA)]
+
+# Crea un gráfico  
+if not df_filtrado.empty:  
+    DIA = df_filtrado['DIA'].tolist()
+    HORA = df_filtrado['HORA'].tolist()
+    
+    plt.plot(DIA, HORA)
+    plt.xlabel("Eje DIA")
+    plt.ylabel("Eje HORA")  
+ 
+    # Muestra el gráfico en Streamlit
+    st.pyplot(plt)
+else:
+    st.write("No hay datos disponibles para los filtros seleccionados.")
